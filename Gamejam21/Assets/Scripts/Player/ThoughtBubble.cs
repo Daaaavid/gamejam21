11f85c3f2614d1825e57ad0395fd7f6d6ae34006
Daaaavid <dvdmrs524@gmail.com>
2021-01-30 15:24:36 +0200

    Thoughtbubble
     and writing in dialoguemanager (again)

---
+++ Gamejam21/Assets/Scripts/Player/ThoughtBubble.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Subtegral.DialogueSystem.DataContainers;
+using System.Linq;
+
+public class ThoughtBubble : MonoBehaviour
+{
+    [SerializeField] private Text thoughtBubbleText;
+    [SerializeField] private Transform thoughtBubble;
+    [SerializeField] public DialogueContainer _dialogue;
+
+    [SerializeField] private DialogueNodeData currentNode;
+    [Tooltip("0 = false, 1 = true")]
+    [Range(0, 1)] public int _splitterValue;
+
+    public void NewThoughtBubble(DialogueContainer dialogue, int splittervalue) {
+        thoughtBubble.gameObject.SetActive(true);
+        thoughtBubbleText.text = "";
+        _dialogue = dialogue;
+        _splitterValue = splittervalue;
+        var narrativeData = dialogue.NodeLinks.First(); //Entrypoint node
+        currentNode = NextNode(narrativeData.TargetNodeGuid);
+        if (currentNode != null)
+            AddThoughtText(currentNode);
+    }
+
+    private DialogueNodeData NextNode(string narrativeDataGUID) {
+        DialogueNodeData node = _dialogue.DialogueNodeData.Find(x => x.Guid == narrativeDataGUID);
+        while (node.type == 3) {
+            var choices = _dialogue.NodeLinks.Where(x => x.BaseNodeGuid == node.Guid);
+            node = _dialogue.DialogueNodeData.Find(x => x.Guid == choices.ElementAt(_splitterValue).TargetNodeGuid);
+        }
+        if (node.type == 0) {
+            thoughtBubble.gameObject.SetActive(false);
+            return null;
+        }
+        return node;
+    }
+
+    private void AddThoughtText(DialogueNodeData node) {
+        switch (node.type) {
+            case 1:
+                StopAllCoroutines();
+                StartCoroutine(TypeSentence(node.DialogueText));
+                break;
+            case 2:
+                Debug.LogError("player dialogue nodes cannot be used in thoughtbubbles.");
+                break;
+        }
+    }
+
+    IEnumerator TypeSentence(string sentence) {
+        thoughtBubbleText.text = "";
+        Debug.Log(sentence);
+        foreach (char letter in sentence.ToCharArray()) {
+            thoughtBubbleText.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        ContinueThoughts();
+
+    }
+
+    void ContinueThoughts() {
+        var choices = _dialogue.NodeLinks.Where(x => x.BaseNodeGuid == currentNode.Guid);
+        currentNode = NextNode(choices.ElementAt(0).TargetNodeGuid);
+        if (currentNode != null)
+        AddThoughtText(currentNode);
+    }
+}
